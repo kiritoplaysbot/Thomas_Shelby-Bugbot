7cb1b52381c4889b80f1d7af5d4e499bae208349
kiritoplaysbot <157273788+kiritoplaysbot@users.noreply.github.com>
2024-06-22 19:57:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,9 +31,9 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
 
@@ -42,7 +42,7 @@ global.autoswview = true //auto view status/story
 //v2 is link + image menu,
 //v3 is video menu,
 //v4 is call end menu
-global.typemenu = 'v2'
+global.typemenu = 'v4'
 
 //text bug
 global.xbugtex = {
